1a3fa6ebabf65ebc165e53929eab2c5ca1a52fa1
Jermain Jennings <jermainejennings05@gmail.com>
2017-10-19 10:06:06 -0600

    deployment issue fixed

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -22,12 +22,12 @@
 		responsible for taking student and sport team photos for several of the local public schools in the city,
 		in addition to doing side projects (weddings photos,family photos,etc.).</p>
 		<p>Recently Melissa has been searching for someone to build a website for her business. She has a business page
-			on Facebook but wants to shift over to something thats more professinal,independent,
-			and has more flexibility. Melissa says she wants something that can give a run down of her photograpy
-			experience and background. She will also want to have differnt pages for things like contact info and n email
+			on Facebook but wants to shift over to something that's more professional,independent,
+			and has more flexibility. Melissa says she wants something that can give a run down of her photography
+			experience and background. She will also want to have different pages for things like contact info and n email
 			submit section. Melissa would also like to have a page on her website where she can showcase work she has done
-			for potentail clients to view and sample. She says if possible she would also like to have a section that
-			displays a calandar with her avalibility for the month.
+			for potential clients to view and sample. She says if possible she would also like to have a section that
+			displays a calendar with her availability for the month.
 		</p>
 	</body>
 </html>
