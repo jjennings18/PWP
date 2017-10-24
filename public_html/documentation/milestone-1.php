85b0e6d9f4ad1abf44519b0c74d20e869bc1722e
Jermain Jennings <jermainejennings05@gmail.com>
2017-10-24 11:21:40 -0600

    HTML indenting edited

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,10 +6,12 @@
 	</head>
 	<body>
 				<p><strong>Purpose:</strong></p>
-				<p>I would like the PWP to help me market myself to possible employers.</p>
+						<p>I would like the PWP to help me market myself to possible employers.</p>
+
 		<p><strong>Audience</strong></p>
-				<p>My primary audience will be small business who are looking to
-					establish more of a digital foot print and get more exposure.</p>
+						<p>My primary audience will be small business who are looking to
+							establish more of a digital foot print and get more exposure.</p>
+
 		<p><strong>Goal</strong></p>
 			<ul>
 				<li>Attract Freelance clients</li>
@@ -17,17 +19,18 @@
 				<li>Increase sales or exposure for clientele</li>
 	</ul>
 		<p><strong>Persona</strong></p>
-		<p>Melissa is a 40 year old professional photographer. Melissa lives in small city where she owns
-		her own photography company. Melissa has established herself so well in the community that she is
-		responsible for taking student and sport team photos for several of the local public schools in the city,
-		in addition to doing side projects (weddings photos,family photos,etc.).</p>
-		<p>Recently Melissa has been searching for someone to build a website for her business. She has a business page
-			on Facebook but wants to shift over to something that's more professional,independent,
-			and has more flexibility. Melissa says she wants something that can give a run down of her photography
-			experience and background. She will also want to have different pages for things like contact info and n email
-			submit section. Melissa would also like to have a page on her website where she can showcase work she has done
-			for potential clients to view and sample. She says if possible she would also like to have a section that
-			displays a calendar with her availability for the month.
+
+				<p>Melissa is a 40 year old professional photographer. Melissa lives in small city where she owns
+				her own photography company. Melissa has established herself so well in the community that she is
+				responsible for taking student and sport team photos for several of the local public schools in the city,
+				in addition to doing side projects (weddings photos,family photos,etc.).</p>
+				<p>Recently Melissa has been searching for someone to build a website for her business. She has a business page
+					on Facebook but wants to shift over to something that's more professional,independent,
+					and has more flexibility. Melissa says she wants something that can give a run down of her photography
+					experience and background. She will also want to have different pages for things like contact info and n email
+					submit section. Melissa would also like to have a page on her website where she can showcase work she has done
+					for potential clients to view and sample. She says if possible she would also like to have a section that
+					displays a calendar with her availability for the month.
 		</p>
 	</body>
 </html>
