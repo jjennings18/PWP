d5a14cf46a0c78cbc2bf281b39f15dbafeeec24a
Jermain Jennings <jermainejennings05@gmail.com>
2017-11-02 09:43:32 -0600

    milestone2 added

--- public_html/documentation/milestone-2.php/milestone2.php
+++ public_html/documentation/milestone-2.php/milestone2.php
@@ -9,6 +9,6 @@
 		I would also like to have a breif but detailed description of my background and training in this field.
 			I then plan on having a section towards the bottom of the page that list skills and services I can offer, and next to that
 		a "contact info" section. Lastly I plan on pulling it all together with a nice background and/or color and a few different style
-		fonts. Lastly I would like to see If I could possibly throw in an animation or transition if possible.</p>
+		fonts. Lastly I would like to see If I could possibly throw in an animation or transition if possible. </p>
 	</body>
 </html>
