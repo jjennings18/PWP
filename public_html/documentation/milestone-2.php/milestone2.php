33d563b5ba36cd2e1e6c769fcd235db777157dd2
Jermain Jennings <jermainejennings05@gmail.com>
2017-11-01 21:53:52 -0600

    milestone2 added

---
+++ public_html/documentation/milestone-2.php/milestone2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>milestone-2.php</title>
+	</head>
+	<body>
+		<p>For my pwp website I plan on taking a simple but elegant approach. I want a nice professional displayed of myself.
+		I would also like to have a breif but detailed description of my background and training in this field.
+			I then plan on having a section towards the bottom of the page that list skills and services I can offer, and next to that
+		a "contact info" section. Lastly I plan on pulling it all together with a nice background and/or color and a few different style
+		fonts. Lastly I would like to see If I could possibly throw in an animation or transition if possible.</p>
+	</body>
+</html>
