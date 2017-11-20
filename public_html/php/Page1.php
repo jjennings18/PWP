46bb49eab75f4acbca5f2488b5a3468d5beda462
Jermain Jennings <jermainejennings05@gmail.com>
2017-11-20 13:27:16 -0700

    Bootstrap4 page added. brief bio added.

---
+++ public_html/php/Page1.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Jermain Jennings</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1></h1>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+	  <div class="jumbotron jumbotron-fluid">
+		  <div class="container">
+			  <h1 class="text-primary" class="display-3">Welcome!</h1>
+			  <p class="lead"></p>
+		  </div>
+	  </div>
+	  <img src="../images/jermainjennings2.jpg"  class="rounded mx-auto d-block" class="img-fluid" alt="Responsive image">
+	  <div
+			 <p class="text-center"> My Name is Jermain Jennings I am a native to Albuquerque, New Mexico. Ive been working with computers for
+				 over 10 years. I am currently looking to help small bussiness owners start or strengthen their digital footprint
+			 in the modern world of technology. My skillset allows me to build basic yet elegant websites that are fully
+			 functional and mobile friendly so your business can be viewd by everyone!</p>
+  </body>
+</html>
