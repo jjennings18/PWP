0f1b4c025ddecd8ecdbf94ab24df5ef586c51f71
Jermain Jennings <jermainejennings05@gmail.com>
2017-11-21 13:46:01 -0700

    sklill list added

--- public_html/php/Page1.php
+++ public_html/php/Page1.php
@@ -17,18 +17,48 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-
+	  <!-- start of page -->
 	  <div class="jumbotron jumbotron-fluid">
 		  <div class="container">
 			  <h1 class="text-primary" class="display-3">Welcome!</h1>
 			  <p class="lead"></p>
 		  </div>
 	  </div>
+	  <!-- Headshot image -->
 	  <img src="../images/jermainjennings2.jpg"  class="rounded mx-auto d-block" class="img-fluid" alt="Responsive image">
-	  <div
+	  <!-- Brief Bio-->
+	  <div class="container">
 			 <p class="text-center"> My Name is Jermain Jennings I am a native to Albuquerque, New Mexico. Ive been working with computers for
 				 over 10 years. I am currently looking to help small bussiness owners start or strengthen their digital footprint
 			 in the modern world of technology. My skillset allows me to build basic yet elegant websites that are fully
 			 functional and mobile friendly so your business can be viewd by everyone!</p>
+	  </div>
+
+	  <div class="card-deck">
+		  <div class="card">
+			  <div class="card-body">
+				  <h4 class="card-title">Skills</h4>
+				  <ul class="card-text">
+					  <li>write HTML and CSS</li>
+					  <li>wireframe/layout a website or web application </li>
+					  <li>write a basic application in an object-oriented programming language</li>
+					  <li>Deploy websites to website host</li>
+				  </ul>
+			  </div>
+		  </div>
+		  <div class="card">
+			  <div class="card-body">
+				  <h4 class="card-title">Card title</h4>
+				  <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+			  </div>
+		  </div>
+		  <div class="card">
+			  <div class="card-body">
+				  <h4 class="card-title">Card title</h4>
+				  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+			  </div>
+		  </div>
+	  </div>
+
   </body>
 </html>
